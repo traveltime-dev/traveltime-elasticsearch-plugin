3522450d9c30ce5b768c8d71e03d473a1e4885e4
aivaras <aivaras@traveltime.com>
2022-01-20 15:54:11 +0200

    changed plugin auth settings to simpleString

--- src/main/java/com/traveltime/plugin/elasticsearch/TraveltimePlugin.java
+++ src/main/java/com/traveltime/plugin/elasticsearch/TraveltimePlugin.java
@@ -7,7 +7,6 @@ import com.traveltime.plugin.elasticsearch.query.TraveltimeQueryParser;
 import com.traveltime.plugin.elasticsearch.util.Util;
 import com.traveltime.sdk.dto.requests.proto.Country;
 import com.traveltime.sdk.dto.requests.proto.Transportation;
-import org.elasticsearch.common.settings.SecureSetting;
 import org.elasticsearch.common.settings.Setting;
 import org.elasticsearch.plugins.Plugin;
 import org.elasticsearch.plugins.SearchPlugin;
@@ -18,8 +17,8 @@ import java.util.List;
 import java.util.Optional;
 
 public class TraveltimePlugin extends Plugin implements SearchPlugin {
-   public static Setting<String> APP_ID = SecureSetting.simpleString("traveltime.app.id", Setting.Property.Consistent);
-   public static Setting<String> API_KEY = SecureSetting.simpleString("traveltime.api.key", Setting.Property.Consistent);
+   public static Setting<String> APP_ID = Setting.simpleString("traveltime.app.id", Setting.Property.NodeScope);
+   public static Setting<String> API_KEY = Setting.simpleString("traveltime.api.key", Setting.Property.NodeScope, Setting.Property.Filtered);
    public static Setting<Optional<Transportation>> DEFAULT_MODE = new Setting<>("traveltime.default.mode", s -> "", Util::findModeByName, Setting.Property.NodeScope);
    public static Setting<Optional<Country>> DEFAULT_COUNTRY = new Setting<>("traveltime.default.country", s -> "", Util::findCountryByName, Setting.Property.NodeScope);
    public static Setting<URI> API_URI = new Setting<>("traveltime.api.uri", s -> "https://proto.api.traveltimeapp.com/api/v2/", URI::create, Setting.Property.NodeScope);
