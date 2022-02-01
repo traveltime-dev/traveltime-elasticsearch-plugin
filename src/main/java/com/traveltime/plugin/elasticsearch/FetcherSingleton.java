1ef9ac70362ef39ba5abe22c616dfec09a105cd6
Marcin Januszkiewicz <marcin.januszkiewicz@traveltime.com>
2022-02-01 14:55:22 +0100

    Bump sdk version and fix field type.

--- src/main/java/com/traveltime/plugin/elasticsearch/FetcherSingleton.java
+++ src/main/java/com/traveltime/plugin/elasticsearch/FetcherSingleton.java
@@ -6,7 +6,7 @@ public enum FetcherSingleton {
    INSTANCE;
 
    private ProtoFetcher underlying = null;
-   private final Object lock = new Object[0];
+   private final Object[] lock = new Object[0];
 
    public ProtoFetcher getFetcher(URI uri, String id, String key) {
       if(underlying != null) return underlying;
