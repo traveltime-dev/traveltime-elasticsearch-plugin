9b5a44a438213bc95431a7c59aa22055e1d182c5
Marcin Januszkiewicz <marcin.januszkiewicz@traveltime.com>
2022-03-08 18:24:27 +0100

    Add support for version 7.17, 8.0, 8.1

---
+++ 8.0/src/main/java/com/traveltime/plugin/elasticsearch/query/TraveltimeSearchQuery.java
@@ -0,0 +1,50 @@
+package com.traveltime.plugin.elasticsearch.query;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.search.*;
+
+import java.io.IOException;
+import java.net.URI;
+
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+@Getter
+public class TraveltimeSearchQuery extends Query {
+   private final TraveltimeQueryParameters params;
+   private final Query prefilter;
+   private final URI appUri;
+   private final String appId;
+   private final String apiKey;
+   private final Integer batchSize;
+
+   @Override
+   public void visit(QueryVisitor visitor) {
+      if (prefilter != null) {
+         prefilter.visit(visitor);
+      }
+   }
+
+   @Override
+   public String toString(String field) {
+      return String.format("TraveltimeSearchQuery(params = %s, prefilter = %s)", params, prefilter);
+   }
+
+   @Override
+   public Weight createWeight(IndexSearcher searcher, ScoreMode scoreMode, float boost) throws IOException {
+      Weight prefilterWeight = prefilter != null ? prefilter.createWeight(searcher, scoreMode, boost) : null;
+      return new TraveltimeWeight(this, prefilterWeight, boost);
+   }
+
+   @Override
+   public Query rewrite(IndexReader reader) throws IOException {
+      Query newPrefilter = prefilter != null ? prefilter.rewrite(reader) : null;
+      if (newPrefilter == prefilter) {
+         return super.rewrite(reader);
+      } else {
+         return new TraveltimeSearchQuery(params, newPrefilter, appUri, appId, apiKey, batchSize);
+      }
+   }
+}
