14d00ce0c26aa2a75452a7ace05dec90c68cb80c
mjanuszkiewicz <marcin.januszkiewicz@traveltime.com>
2022-04-04 12:12:26 +0100

    Update 8.1 version list

--- common/src/universal/wait-for-startup.sh
+++ common/src/universal/wait-for-startup.sh
@@ -2,7 +2,7 @@
 
 START=$(date +%s)
 
-until $(curl --output /dev/null --silent --fail http://127.0.0.1:9200/_cat/health); do
+until curl --output /dev/null --silent --fail http://127.0.0.1:9200/_cat/health; do
     NOW=$(date +%s)
     if [ $(expr ${NOW} - ${START}) -ge 60 ];then
       echo "Timeout"
