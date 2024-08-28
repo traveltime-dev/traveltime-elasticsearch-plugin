bf17e669cba3bf80d59921d8dc22a85c3278926b
Marcin Januszkiewicz <marcin.januszkiewicz@traveltime.com>
2024-08-28 16:51:08 +0200

    Implement distance queries for version 8.7-8.9

--- 8.7/src/main/java/com/traveltime/plugin/elasticsearch/query/TraveltimeFetchPhase.java
+++ 8.7/src/main/java/com/traveltime/plugin/elasticsearch/query/TraveltimeFetchPhase.java
@@ -47,6 +47,7 @@ public class TraveltimeFetchPhase implements FetchSubPhase {
       if (traveltimeQuery == null) return null;
       TraveltimeQueryParameters params = traveltimeQuery.getParams();
       final String output = traveltimeQuery.getOutput();
+      final String distanceOutput = traveltimeQuery.getDistanceOutput();
 
       FieldFetcher fieldFetcher = FieldFetcher.create(fetchContext.getSearchExecutionContext(), List.of(new FieldAndFormat(params.getField(), null)));
 
@@ -61,10 +62,18 @@ public class TraveltimeFetchPhase implements FetchSubPhase {
          public void process(HitContext hitContext) throws IOException {
             val docValues = hitContext.reader().getSortedNumericDocValues(params.getField());
             docValues.advance(hitContext.docId());
-            Integer tt = TraveltimeCache.INSTANCE.get(params, docValues.nextValue());
+            if (!output.isEmpty()) {
+               Integer tt = TraveltimeCache.INSTANCE.get(params, docValues.nextValue());
+               if (tt >= 0) {
+                  hitContext.hit().setDocumentField(output, new DocumentField(output, List.of(tt)));
+               }
+            }
 
-            if (tt >= 0) {
-               hitContext.hit().setDocumentField(output, new DocumentField(output, List.of(tt)));
+            if (!distanceOutput.isEmpty()) {
+               Integer td = TraveltimeCache.DISTANCE.get(params, docValues.nextValue());
+               if (td >= 0) {
+                  hitContext.hit().setDocumentField(distanceOutput, new DocumentField(distanceOutput, List.of(td)));
+               }
             }
          }
 
